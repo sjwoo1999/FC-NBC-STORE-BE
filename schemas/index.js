ab6a4ff5d22a5881ab291fea7a13e88f4bccb7be
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-01-21 13:08:33 +0900

    .env / .gitignore file added

--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
+require("dotenv").config(); // .env 파일에서 환경변수 불러오기
+
 const connect = () => {
   // mongoose.connect는 MongoDB 서버에 연결하는 메서드입니다.
   mongoose
     .connect(
       // 빨간색으로 표시된 부분은 대여한 ID, Password, 주소에 맞게끔 수정해주세요!
-      "mongodb+srv://sparta-user:aaaa4321@express-mongo.uy7ttg7.mongodb.net/?retryWrites=true&w=majority",
+      process.env.MONGODB_URI,
       {
         dbName: "node_lv1", // node_lv1 데이터베이스명을 사용합니다.
       }
