08aee8b0681e7a1578a10f304201e4d496942abc
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-01-22 11:39:59 +0900

    added .prettierrc

--- schemas/index.js
+++ schemas/index.js
@@ -5,13 +5,9 @@ require("dotenv").config(); // .env 파일에서 환경변수 불러오기
 const connect = () => {
   // mongoose.connect는 MongoDB 서버에 연결하는 메서드입니다.
   mongoose
-    .connect(
-      // 빨간색으로 표시된 부분은 대여한 ID, Password, 주소에 맞게끔 수정해주세요!
-      process.env.MONGODB_URI,
-      {
-        dbName: "node_lv1", // node_lv1 데이터베이스명을 사용합니다.
-      }
-    )
+    .connect(process.env.MONGODB_URI, {
+      dbName: "node_lv1", // node_lv1 데이터베이스명을 사용합니다.
+    })
     .then(() => console.log("MongoDB 연결에 성공하였습니다."))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
