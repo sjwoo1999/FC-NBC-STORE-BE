e1382428e09a63570ca3e3d576e3a1003a60dc68
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-01-22 22:46:53 +0900

    api request | products.router.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import productsRouter from "./routes/products.router.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,13 +8,14 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// router setting
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.json({ message: "Hi!" });
+  return res.json({ message: "Hi! 하이루" });
 });
 
-app.use("/api", router);
+app.use("/api", [router, productsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
---
+++ routes/products.router.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const router = express.Router();
+
+// ---------------------------------------
+// 상품 작성 API | POST : 새로운 데이터 생성
+
+router.post("/create", async (req, res, next) => {
+  // POST 요청을 처리하는 코드
+  return res.json({ message: "POST 요청!" });
+});
+
+// app.use("/create", router);
+// ---------------------------------------
+
+// ---------------------------------------
+// 상품 목록 조회 API | GET
+
+router.get("/getList", async (req, res, next) => {
+  // GET 요청을 처리하는 코드
+  return res.json({ message: "GET 요청!" });
+});
+
+// ---------------------------------------
+
+// 상품 상세 조회 API | GET=
+router.get("/getDetail", async (req, res, next) => {
+  // GET 요청을 처리하는 코드
+  return res.json({ message: "GET 요청!" });
+});
+// ---------------------------------------
+
+// 상품 정보 수정 API | PUT : 기존 데이터 수정
+router.put("/edit", async (req, res, next) => {
+  // PUT 요청을 처리하는 코드
+  return res.json({ message: "PUT 요청!" });
+});
+// ---------------------------------------
+
+// 상품 삭제 API | DELETE
+router.delete("/delete", async (req, res, next) => {
+  // DELETE 요청을 처리하는 코드
+  return res.json({ message: "DELETE 요청!" });
+});
+// ---------------------------------------
+
+export default router;
